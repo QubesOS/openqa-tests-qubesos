c5b9558f84b77dded78b8c3608bd6819ab1b6f10
marmarta <marmarta@invisiblethingslab.com>
2019-03-28 15:49:33 +0100

    Tests for desktop-linux-manager.qui.tray.devices

---
+++ tests/qui_widgets_devices.pm
@@ -0,0 +1,57 @@
+
+# The Qubes OS Project, https://www.qubes-os.org/
+#
+# Copyright (C) 2019 Marta Marczykowska-Górecka <marmarta@invisiblethingslab.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use base "installedtest";
+use strict;
+use testapi;
+
+
+sub run {
+	# open global-settings
+    	select_console('x11');
+	assert_screen "desktop";
+
+        # check if widget opened, open it
+        assert_and_click('qui-devices-open', 'left', 20);
+        assert_screen('qui-devices-opened');
+
+        # close and open again
+        send_key('esc');
+        assert_and_click('qui-devices-open', 'left', 20);
+
+        # open a device
+        send_key('up');
+        assert_screen('qui-devices-dev-opened', 'left', 20);
+        
+        # close the widget
+        send_key('esc');
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+    select_console('x11');
+    send_key('esc');
+    save_screenshot;
+    $self->SUPER::post_fail_hook;
+
+};
+
+1;
+
+# vim: set sw=4 et:
+

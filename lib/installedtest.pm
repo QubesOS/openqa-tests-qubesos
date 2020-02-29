91c68b9b62ad5125507758316a8fdd3e7f0eac67
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-02-29 10:14:00 -0700

    Adjust settings to work on Intel workers too
    Enable both svm and vmx - qemu will complain one of them is missing, but
    otherwise will enable the right one.
    
    Also increase some timeouts and disable some of the side-channel attacks
    mititagions - those hit Intel much more than AMD and together with nested
    virtualization makes the VMs almost unusably slow.
    With just Xen mitigations disabled, it is still slower than on AMD, but seems
    to be good enough to run tests.

--- lib/installedtest.pm
+++ lib/installedtest.pm
@@ -46,7 +46,7 @@ sub handle_system_startup {
     }
 
     # handle both encrypted and unencrypted setups
-    assert_screen ["luks-prompt", "login-prompt-user-selected"], 240;
+    assert_screen ["luks-prompt", "login-prompt-user-selected"], 600;
     if (match_has_tag('luks-prompt')) {
         type_string "lukspass";
         send_key "ret";
--- tests/install_fixups.pm
+++ tests/install_fixups.pm
@@ -47,16 +47,17 @@ sub run {
         type_string "sed -ie '$sed_expr' $core2_path\n";
     }
     type_string "exit\n";
-    script_run "sed -i -e 's:console=none:console=vga,com1 loglvl=all guest_loglvl=all:' /mnt/sysimage/boot/grub2/grub.cfg";
-    script_run "sed -i -e 's:console=none:console=vga,com1 loglvl=all guest_loglvl=all:' /mnt/sysimage/boot/efi/EFI/qubes/grub.cfg";
+    my $extra_xen_opts = 'loglvl=all guest_loglvl=all spec-ctrl=no';
+    script_run "sed -i -e 's:console=none:console=vga,com1 $extra_xen_opts:' /mnt/sysimage/boot/grub2/grub.cfg";
+    script_run "sed -i -e 's:console=none:console=vga,com1 $extra_xen_opts:' /mnt/sysimage/boot/efi/EFI/qubes/grub.cfg";
     script_run "sed -i -e 's:\\\${xen_rm_opts}::' /mnt/sysimage/boot/efi/EFI/qubes/grub.cfg";
     my $xen_cfg = '/mnt/sysimage/boot/efi/EFI/qubes/xen.cfg';
     if (!script_run("grep console= $xen_cfg")) {
-        script_run "sed -i -e 's:console=none:console=vga,com1 loglvl=all guest_loglvl=all:' $xen_cfg";
+        script_run "sed -i -e 's:console=none:console=vga,com1 $extra_xen_opts:' $xen_cfg";
     } else {
-        script_run "sed -i -e 's:^options=:options=console=vga,com1 loglvl=all guest_loglvl=all :' $xen_cfg";
+        script_run "sed -i -e 's:^options=:options=console=vga,com1 $extra_xen_opts :' $xen_cfg";
     }
-    script_run "sed -i -e 's:console=none:console=vga,com1 loglvl=all guest_loglvl=all:' /mnt/sysimage/etc/default/grub";
+    script_run "sed -i -e 's:console=none:console=vga,com1 $extra_xen_opts:' /mnt/sysimage/etc/default/grub";
 
     # need to use explicit UUID to override (empty) options from /etc/crypttab,
     # rd.luks.options=discard works only for disks not mentioned in

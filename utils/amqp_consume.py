5276720e3af3b9725b4f64aa0605e13cbfc4e571
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-03-15 05:21:13 -0700

    Add automatic job restart
    Sometimes test fails in an initial stage, because of the console/vnc
    unreliability. Automatically restart such job up to 3 times.

--- utils/amqp_consume.py
+++ utils/amqp_consume.py
@@ -4,6 +4,7 @@ import os.path
 import pika
 import json
 import argparse
+import collections
 import requests
 import subprocess
 
@@ -15,18 +16,55 @@ API_BASE = 'https://openqa.qubes-os.org/api/v1/'
 
 args = None
 
+# list of test groups to automatically restart a job if fails
+# 'shutdown' is added only to system_tests_update and is needed to upload disk image
+RESTART_ON_GROUP_FAIL = ('startup_fixup', 'whonix_firstrun', 'update', 'shutdown')
+RESTART_LIMIT = 3
+
+job_restarts = collections.defaultdict(lambda: 0)
+
+def maybe_restart_failed_job(job_id, job_details):
+    """Restart failed job if the failure most likely is caused by openQA
+       unusual environment. Returns True if the job was restarted"""
+    restart = False
+    for group in job_details['job']['testresults']:
+        if group['result'] == 'passed':
+            continue
+        if group['name'] not in RESTART_ON_GROUP_FAIL:
+            continue
+        restart = True
+    if not restart:
+        return False
+
+    restart_id = job_details['job']['settings']['BUILD'] + '_' + job_details['job']['settings']['TEST']
+    if job_restarts[restart_id] >= RESTART_LIMIT:
+        print('Job {} restarted {} times already, not restarting again'.format(
+                  restart_id, job_restarts[restart_id]))
+        return False
+    job_restarts[restart_id] += 1
+
+    print('Restarting job {} ({})'.format(job_id, restart_id))
+    subprocess.call([
+        'openqa-client', '--host', API_BASE.partition('/api/')[0],
+        'jobs/{}/restart'.format(job_id), 'post'])
+    return True
+
 def callback_done(ch, method, properties, body):
     print('received done, properties %r, body %r' % (
         properties, body))
     job_data = json.loads(body)
-    if job_data['remaining'] > 0:
-        print('group not done yet done')
-        return
-    r = requests.get('{}/jobs/{}'.format(API_BASE, job_data['id']))
+    r = requests.get('{}/jobs/{}/details'.format(API_BASE, job_data['id']))
     if not r.ok:
         print('failed to get job {} info: {}'.format(job_data['id'], r.text))
         return
     job_details = r.json()
+
+    if maybe_restart_failed_job(job_data['id'], job_details):
+        return
+
+    if job_data['remaining'] > 0:
+        print('group not done yet')
+        return
 
     version = job_details['job']['settings']['VERSION']
 
@@ -45,6 +83,7 @@ def callback_done(ch, method, properties, body):
                     base_job = base_jobs[version]
         if base_job:
             cmd.extend(['--compare-to-job', str(base_job)])
+        print('Calling: {}'.format(' '.join(cmd)))
         subprocess.call(cmd)
 
 

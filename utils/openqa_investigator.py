6824725b12c55da5b081290b31ff89d96c48fab4
deeplow <deeplower@protonmail.com>
2021-07-27 07:44:37 -0400

    openqa investigator: specify test suite via args

--- utils/openqa_investigator.py
+++ utils/openqa_investigator.py
@@ -5,10 +5,9 @@ from fnmatch import fnmatch
 
 HISTORY_LEN = 200
 Q_VERSION = "4.1"
-TEST_SUITE_NAME = "system_tests_network_ipv6"
 FLAVOR = "pull-requests"
 
-def print_tests_failures(test_name, test_title):
+def print_tests_failures(test_suite, test_name, test_title):
     """
     Prints the historical data of a particular of job failures for a
     particular test pattern
@@ -18,22 +17,23 @@ def print_tests_failures(test_name, test_title):
     print("\tLooking for failures of test {}/{}".format(test_name,
                                                           test_title))
     print("\ton the last {} failed tests".format(HISTORY_LEN))
+    print("\nsuite: ", test_suite)
 
-    jobs = OpenQA.get_latest_job_ids(TEST_SUITE_NAME, version=Q_VERSION,
+    jobs = OpenQA.get_latest_job_ids(test_suite, version=Q_VERSION,
                                      n=HISTORY_LEN, result="failed",
                                      flavor=FLAVOR)
 
     for job_id in jobs:
         job = JobData(job_id)
-        print_test_failure(job, test_name, test_title)
+        print_test_failure(job, test_suite, test_name, test_title)
 
-def print_test_failure(job, test_name, test_title):
+def print_test_failure(job, test_suite, test_name, test_title):
     """
     Prints the failures of a particular test pattern
     """
 
     result = job.get_results()
-    test_failures = result[TEST_SUITE_NAME]
+    test_failures = result[test_suite]
 
     print("\n## Job {} (flavor '{}' from {})".format(job.job_id,
                                                     job.get_job_flavor(),
@@ -55,6 +55,11 @@ def main():
         description="Look for unstable tests")
 
     parser.add_argument(
+        "--suite",
+        help="Test suite name"
+             "(e.g.: system_tests_splitgpg)")
+
+    parser.add_argument(
         "--test",
         help="Test Case with wildcard support"
              "(e.g.: TC_00_Direct_*/test_000_version)")
@@ -68,7 +73,7 @@ def main():
         test_title = "*"
 
     if args.test:
-        print_tests_failures(test_name, test_title)
+        print_tests_failures(args.suite, test_name, test_title)
 
 
 if __name__ == '__main__':

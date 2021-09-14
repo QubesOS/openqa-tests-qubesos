1e53832cb5be1d99bc0fd03f99ba1ef2ca8babaa
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-09-14 12:40:50 +0000

    Better logging

--- lib/installedtest.pm
+++ lib/installedtest.pm
@@ -99,7 +99,7 @@ sub post_fail_hook {
     script_run "cat /var/log/salt/minion";
     script_run "cat /var/log/libvirt/libxl/libxl-driver.log";
     script_run "tail /var/log/xen/console/guest*-dm.log";
-    script_run "tail -n 30 /var/log/xen/console/guest*.log";
+    script_run "grep -B 50 'Kernel panic' /var/log/xen/console/guest*.log";
     enable_dom0_network_netvm() unless $self->{network_up};
     upload_logs('/var/log/libvirt/libxl/libxl-driver.log');
     $self->save_and_upload_log('journalctl -b', 'journalctl.log', {timeout => 120});

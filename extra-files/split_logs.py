4c2f4995ed187790c3cc412ef5bf2574a0419ada
deeplow <deeplower@protonmail.com>
2021-12-24 03:34:03 -0500

    make path concatenation more resilient

--- extra-files/split_logs.py
+++ extra-files/split_logs.py
@@ -27,7 +27,7 @@ def get_logs(path):
     for file_name in os.listdir(path):
         if not file_name.startswith("guest-test-"):
             continue
-        with open(path + file_name, 'r') as f:
+        with open(os.path.join(path, file_name), 'r') as f:
             logs[file_name] = f.readlines()
     return logs
 

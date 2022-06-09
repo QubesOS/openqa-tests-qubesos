52791526d85d86f92d2275a6c762b76830c5eb7c
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2022-06-09 16:27:43 +0000

    Skip whonixcheck early, if whonix wasn't selected for testing
    Do not open xterm window at all.

--- tests/whonixcheck.pm
+++ tests/whonixcheck.pm
@@ -23,15 +23,15 @@ use networking;
 sub run {
     my ($self) = @_;
 
-    select_console('x11');
-    assert_screen "desktop";
-    x11_start_program('xterm');
-    curl_via_netvm;
-
     if (get_var('TEST_TEMPLATES') && !(get_var("TEST_TEMPLATES") =~ m/whonix/)) {
         record_info('skip', "only selected templates test requested and whonix is not one of them");
         return;
     }
+
+    select_console('x11');
+    assert_screen "desktop";
+    x11_start_program('xterm');
+    curl_via_netvm;
 
     my @whonix_vms = split(/\n/, script_output("qvm-ls --raw-list|grep whonix"));
     # sort sys-whonix first

cb57365e7e0e523ee4e178977141b89169cb09c5
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2024-05-09 16:45:07 +0000

    Allow PR-originating builds for grub2 too -> via "kernel" flavor

--- api/gitlab_api.py
+++ api/gitlab_api.py
@@ -28,7 +28,7 @@ TARGET_ISO_DIR = '/var/lib/openqa/factory/iso'
 # defaults
 config_defaults = {
     'owner_allowlist': 'QubesOS',
-    'repo_allowlist': 'qubes-continuous-integration qubes-installer-qubes-os qubes-linux-kernel qubes-vmm-xen qubes-vmm-xen-stubdom-linux qubes-gui-agent-linux',
+    'repo_allowlist': 'qubes-continuous-integration qubes-installer-qubes-os qubes-linux-kernel qubes-vmm-xen qubes-vmm-xen-stubdom-linux qubes-gui-agent-linux qubes-grub2',
     'repo_blocklist': None,
     'job_allowlist': '*',
     'user_allowlist': None,
@@ -261,6 +261,7 @@ def run_test_pr(comment_details):
     if pr_details['base']['repo']['name'] in (
             'qubes-linux-kernel',
             'qubes-gui-agent-linux',
+            'qubes-grub2',
             'qubes-vmm-xen',
             'qubes-vmm-xen-stubdom-linux'):
         values['FLAVOR'] = 'kernel'

5e85e12c3d7339dcc584183de6a422b386065aba
Rowen S <ro@freedom.press>
2025-08-20 14:57:27 -0400

    Refactor install method to use keyring boostrapping
    Supports new installation logic provided in
    https://github.com/freedomofpress/securedrop-workstation/pull/1416
    
    Additionally, this adds support for staging installations (even though
    this is currently not in use)

--- tests/securedrop/install_workstation.pm
+++ tests/securedrop/install_workstation.pm
@@ -16,8 +16,7 @@ use strict;
 use testapi;
 use networking;
 
-
-sub install_bootstrap_dev {
+sub download_repo {
     # Assumes terminal window is open
     # Assumes "curl_via_netvm"
 
@@ -29,32 +28,25 @@ sub install_bootstrap_dev {
     assert_script_run("curl -f -L -o - $repo_archive_url" . get_var('GIT_REF') . '.zip > sdw.zip');
     assert_script_run('unzip sdw.zip');
     assert_script_run('mv securedrop-workstation-* securedrop-workstation');
+};
+
+sub install_dev {
+    download_repo();
+    build_rpm();
+    assert_script_run('mv /tmp/sdw.rpm securedrop-workstation/rpm-build/RPMS/');
     assert_script_run('cd securedrop-workstation && make bootstrap-dev');
 };
 
 
 sub install_staging {
-    # Assumes terminal window is open
+    download_repo();
 
-    # NOTE: These are done via qvm-run instead of gnome-terminal so that we
-    # can know in case they failed.
-    assert_script_run('qvm-run -p work -- gpg --keyserver hkps://keys.openpgp.org --recv-key "2359 E653 8C06 13E6 5295 5E6C 188E DD3B 7B22 E6A3"');
-    assert_script_run('qvm-run -p work -- "gpg --armor --export 2359E6538C0613E652955E6C188EDD3B7B22E6A3 > securedrop-release-key.pub"');
-    assert_script_run('qvm-run -p work -- sudo rpmkeys --import securedrop-release-key.pub');
-    assert_script_run('qvm-run -p work -- "echo -e \"[sd]\nenabled=1\nbaseurl=https://yum-qa.securedrop.org/workstation/dom0/f37\nname=boostrap\"  | sudo tee /etc/yum.repos.d/securedrop-temp.repo"');
-    assert_script_run('qvm-run -p work -- dnf download -y securedrop-workstation-dom0-config');
-    assert_script_run('qvm-run -p work -- "rpm -Kv securedrop-workstation-dom0-config-*.rpm"');  # TODO confirm output is correct
-    assert_script_run('qvm-run -p work -- "cat /home/user/securedrop-workstation-dom0-config-*.rpm" > securedrop-workstation.rpm');
-    assert_script_run('sudo dnf -y install securedrop-workstation.rpm');
-
-    # setup staging config.json
-    assert_script_run('echo {\"submission_key_fpr\": \"65A1B5FF195B56353CC63DFFCC40EF1228271441\", \"hidserv\": {\"hostname\": \"bnbo6ryxq24fz27chs5fidscyqhw2hlyweelg4nmvq76tpxvofpyn4qd.onion\", \"key\": \"FDF476DUDSB5M27BIGEVIFCFGHQJ46XS3STAP7VG6Z2OWXLHWZPA\"}, \"environment\": \"staging\", \"vmsizes\": {\"sd_app\": 10, \"sd_log\": 5}} | sudo tee /usr/share/securedrop-workstation-dom0-config/config.json');
-    assert_script_run('curl https://raw.githubusercontent.com/freedomofpress/securedrop/d91dc67/securedrop/tests/files/test_journalist_key.sec.no_passphrase | sudo tee /usr/share/securedrop-workstation-dom0-config/sd-journalist.sec');
-    assert_script_run('sdw-admin --validate');
-
+    # `make staging` installs from yum-test
+    assert_script_run('cd securedrop-workstation && make bootstrap-staging');
 };
 
-sub install_dev {
+
+sub build_rpm {
     # Assumes terminal window is open
 
 
@@ -85,14 +77,8 @@ sub install_dev {
 
     assert_script_run('qvm-run -p sd-dev "cd securedrop-workstation && make build-rpm"', timeout => 1000);
     assert_script_run("qvm-run --pass-io sd-dev 'cat /home/user/securedrop-workstation/rpm-build/RPMS/noarch/*.rpm' > /tmp/sdw.rpm");
-    assert_script_run('sudo dnf -y install /tmp/sdw.rpm', timeout => 1000);
-
-    # setup dev config.json
-    assert_script_run('echo {\"submission_key_fpr\": \"65A1B5FF195B56353CC63DFFCC40EF1228271441\", \"hidserv\": {\"hostname\": \"bnbo6ryxq24fz27chs5fidscyqhw2hlyweelg4nmvq76tpxvofpyn4qd.onion\", \"key\": \"FDF476DUDSB5M27BIGEVIFCFGHQJ46XS3STAP7VG6Z2OWXLHWZPA\"}, \"environment\": \"dev\", \"vmsizes\": {\"sd_app\": 10, \"sd_log\": 5}} | sudo tee /usr/share/securedrop-workstation-dom0-config/config.json');
-    assert_script_run('curl https://raw.githubusercontent.com/freedomofpress/securedrop/d91dc67/securedrop/tests/files/test_journalist_key.sec.no_passphrase | sudo tee /usr/share/securedrop-workstation-dom0-config/sd-journalist.sec');
-    assert_script_run('sdw-admin --validate');
-
 };
+
 
 sub run {
     my ($self) = @_;
@@ -107,7 +93,6 @@ sub run {
 
     assert_script_run('set -o pipefail'); # Ensure pipes fail\
 
-    install_bootstrap_dev;
     install_dev;
 
     assert_script_run('env xset -dpms; env xset s off', valid => 0, timeout => 10); # disable screen blanking during long command
